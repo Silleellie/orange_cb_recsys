72a29ab3d4009dc2e5b30204d3126f66d71447c4
MarcoPolignano <marpolignano@alice.it>
2021-01-04 21:26:42 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='orange_cb_recsys',
-      version='0.3.5',
+      version='0.3.9',
       author='Roberto Barile, Francesco Benedetti, Carlo Parisi, Mattia Patruno',
       install_requires=[
           'pandas==1.0.5',
@@ -30,5 +30,5 @@ setup(name='orange_cb_recsys',
       long_description_content_type="text/markdown",
       url='https://github.com/m3ttiw/orange_cb_recsys',
       packages=setuptools.find_packages(),
-      python_requires='>=3.8'
+      python_requires='>=3.5'
       )
